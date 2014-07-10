89d5e1e212cf69b8a4b23a20758005ef97a75268
Log00 <q961952819@gmail.com>
2014-07-10 12:40:05 +0800

    Update auth.php

--- server/auth.php
+++ server/auth.php
@@ -1,5 +1,5 @@
 <?php
-/***(!isset($_SERVER['PHP_AUTH_USER'])) {
+!isset($_SERVER['PHP_AUTH_USER'])) {
     header('WWW-Authenticate: Basic realm="Secret"');
     header('HTTP/1.0 401 Unauthorized');
     echo("oops");
@@ -7,5 +7,5 @@
 } elseif ($_SERVER['PHP_AUTH_USER'] != 'username' || $_SERVER['PHP_AUTH_PW'] != 'password') {
     echo("wrong answer");
     exit;
-    ***/
+    
 }
